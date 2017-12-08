7789c1eaca600ef44c7911f10d0dc0fa2b0a6606
pi <dapiguy3@gmail.com>
2017-12-07 21:21:30 -0600

    fixed eval statement

--- enemie-spawn.py
+++ enemie-spawn.py
@@ -1,12 +1,10 @@
 import random
 import time
 class orcs:
-    orc_basic={"hp":25, "weapon":(darts,knife)}
-    orc_archer={"hp":15, "weapon":(darts,bow)}
+    orc_basic={"hp":25, "weapon":(random.choice(darts,knife))}
+    orc_archer={"hp":15, "weapon":(random.choice(darts,bow))}
     def make_enemie(caste, number):
         for b in range(0,number):
             if caste=="basic":
                 new_bot={}
-                new_bot="orc_bot_"+str(n)+"={}"
-                eval(new_bot)
-                "=orc_basic+
+                eval("orc_bot_"+str(n)+"={})
